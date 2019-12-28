42d9d72884be20d3f083502530d7693366476c07
MadCow234 <madcow234@gmail.com>
2019-12-28 15:59:26 -0600

    Add @types/jest && Add second example test

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('displays proper description', () => {
+  const { getByText } = render(<App />);
+  const descriptionText = getByText(/Deployed with AWS CodePipeline and hosted on AWS Elastic Beanstalk./);
+  expect(descriptionText).toBeInTheDocument();
+});
